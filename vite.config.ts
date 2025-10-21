2540bfe8b274b72ff3345166e022097c91e91e44
Drikus Roor <drikusroor@gmail.com>
2025-10-21 13:35:01 +0200

    feat: add GitHub Actions workflow for deploying to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: "/ratio-d/",
 	plugins: [react(), tailwindcss()],
 	optimizeDeps: {
 		exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"],
