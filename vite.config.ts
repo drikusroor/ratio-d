b97ded114c9876bac1bd149a5be40565d3623115
Drikus Roor <drikusroor@gmail.com>
2025-10-21 11:59:45 +0200

    feat: add Tailwind CSS integration and remove unused styles

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
@@ -9,5 +11,7 @@ export default defineConfig({
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
+    tailwindcss(),
+
   ],
 })
